e75a15de27875a142c0248b5dd654da53b81570d
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-02-17 18:56:38 -0300

    curso web design sessão25 aula246 exe5

--- programming_logic/session25_dom/script/aula241_exe4.js
+++ programming_logic/session25_dom/script/aula241_exe4.js
@@ -1,4 +1,7 @@
 var $txtName = document.querySelector('#messageTxt');
 var name_ = 'Joe';
 var lastName = 'West';
+
 document.querySelector('#messageTxt').value = `${name_} ${lastName}`;
+// document.querySelector('#messageTxt').disabled = true;
+document.querySelector('#messageTxt').readOnly = true;
