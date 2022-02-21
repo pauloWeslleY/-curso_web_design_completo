1751ea5a4a9cc0e86b8111503be9dfae55123f2b
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-02-21 15:29:17 -0300

    curso web design sessão26_Object aula251

---
+++ programming_logic/session26_object/script/aula251.js
@@ -0,0 +1,14 @@
+let myStr = 'Joe';
+
+let test = new String('my string')
+console.log(test);
+console.log(test.valueOf());
+console.log(new Number(2));
+console.log(new Boolean(true));
+
+var arr = new Array(1, 2, 3);
+console.log(arr);
+let obj = new Object();
+obj.title = 'tittle';4
+
+console.log(obj);
