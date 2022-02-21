eb94886d343fb4964c5db92365809a7a0debc231
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-02-21 17:35:48 -0300

    curso web design sessão26_Object aula254

--- programming_logic/session26_object/script/aula253.js
+++ programming_logic/session26_object/script/aula253.js
@@ -1,3 +1,8 @@
+/*
+   *
+   ! ==> AULA 253 |> Operador new
+   *
+*/
 let today = new Date();
 
 let x = [0];
@@ -14,10 +19,35 @@ console.log('<== <= === >= ==>');
 
 let n = 0;
 function changeN(n) {
-   debugger
    let n2 = n;
    console.log(++n2);
 }
 
 changeN(n);
 console.log(n);
+
+console.log('<== <= === >= ==>');
+// line
+/*
+   *
+   fixme ==> AULA 254 |> Objetos Customizados
+   *
+*/
+function changedColor(colorX) {
+   console.log(this);
+   this.color = colorX;
+}
+
+let pen = {
+   color: 'red',
+   changeColor: changedColor
+}
+
+let pen1 = {
+   color: 'blue',
+   changeColor: changedColor
+}
+
+pen.changeColor('black');
+console.log(pen);
+console.log(pen1);
