2a63be20d882b47fb4c6db154f5d1f0c3ad793e7
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-02-21 19:27:09 -0300

    curso web design sessão26_Object aula258

---
+++ programming_logic/session26_object/script/aula258_ex.js
@@ -0,0 +1,56 @@
+/*
+
+   fixme ==> Desafio AULA 258
+
+*/
+
+var people = [
+   person1 = {
+      name: 'Barry',
+      lastName: 'Allen'
+   },
+   person2 = {
+      name: 'Joe',
+      lastName: 'West'
+   },
+   person3 = {
+      name: 'Wally',
+      lastName: 'West'
+   }
+]
+
+for (let i = 0; i <= 2; i++) {
+   console.log(`${people[i].name} ${people[i].lastName}`);
+}
+
+/*
+
+   ! Resolução do Desafio |> AULA 258
+
+*/
+console.log('<== <= === >= ==>');
+// line
+var person1 = {
+   name: 'Iris',
+   lastName: 'West'
+};
+var person2 = {
+   name: 'Linda',
+   lastName: 'Park'
+};
+var person3 = {
+   name: 'Patty',
+   lastName: 'Spivot'
+};
+
+var people1 = [
+   person1,
+   person2,
+   person3
+]
+
+var x = 0;
+while (people1[x]) {
+   console.log(`${people1[x].name} ${people1[x].lastName}`);
+   x++;
+}
