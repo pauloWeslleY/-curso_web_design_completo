1598962dd76bfdcaf42bfc9e3869c42a3ca4bdd5
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-02-21 14:28:02 -0300

    curso web design sessão25 aula248 exe5

---
+++ programming_logic/session25_dom/script/aula246_ex5II.js
@@ -0,0 +1,48 @@
+/*
+   *
+   fixme |> AULA 247 <|
+   ! >= exe 5 DOM
+   *
+*/
+
+function calcAverage() {
+   var all = 0;
+   var args = arguments.length;
+   var x = 0;
+   // while(arguments[x]){}
+   while (typeof arguments[x] === 'number') {
+      all += arguments[x];
+      x++;
+   }
+   return all / args;
+}
+
+console.log('<== <= === >= ==>');
+// Line
+
+(function () {
+   'use strict';
+   var trs = document.querySelectorAll('.js-table tbody .students');
+   var x = 0;
+   while (trs[x]) {
+      var lineStudents = trs[x];
+      var tds = lineStudents.querySelectorAll('td');
+      var notes = recoveryNumbers(tds);
+
+      var average = calcAverage.apply(this, notes);
+      lineStudents.querySelector('.students-m').textContent = average;
+      x++;
+   }
+
+   function recoveryNumbers(tds) {
+      var x = 0;
+      var arr = [];
+      while (tds[x]) {
+         if ( !isNaN(tds[x].textContent) && tds[x].textContent !== '' ) {
+            arr.push(parseFloat(tds[x].textContent));
+         }
+         x++;
+      }
+      return arr;
+   }
+})()
