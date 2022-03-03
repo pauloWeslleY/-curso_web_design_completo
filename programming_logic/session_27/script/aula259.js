c123051606ac13159403783d659c97bb784b06a0
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-03-03 01:17:41 -0300

    curso web design sessão27_Event aula260

--- programming_logic/session_27/script/aula259.js
+++ programming_logic/session_27/script/aula259.js
@@ -1,9 +1,26 @@
 let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
 
-btn2.onclick = handle_Click();
+btn2.onclick = handle_Click;
 function handle_Click() {
    alert('Click!');
    return function () {
       alert('Click2!');
    }
+}
+
+btn3.addEventListener('click', function () {
+   alert('handleClick');
+});
+
+btn3.addEventListener('click', function () {
+   alert('handleClick for 2 times');
+});
+
+btn3.onclick = function () {
+   alert('Hello World!');
+}
+
+btn3.onclick = function () {
+   alert('Click here!');
 }
