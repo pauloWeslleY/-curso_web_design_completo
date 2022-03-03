e1a8c66dc14ca2d0a8d0a7312f1cd4cd3f288378
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-03-03 08:51:14 -0300

    curso web design sessão27_Event aula262

--- programming_logic/session_27/script/aula261.js
+++ programming_logic/session_27/script/aula261.js
@@ -1,15 +1,19 @@
 let $d1 = document.querySelector('#d1');
 let $d1Span = document.querySelector('#d1 span');
 
-$d1.addEventListener('click', function () {
+$d1.addEventListener('click', function (e) {
    console.log('click of D1');
-});
+   e.stopPropagation();
+}, false);
 
 $d1Span.addEventListener('click', function () {
    console.log('click of d1Span');
-});
+}, false);
 
 document.addEventListener('click', (event) => {
-   console.log('Click in Document!');
-});
+   console.log('Click in Here document');
+   console.log(event.currentTarget);
+   console.log(this);
+   console.log(event.target);
+}, false);
 
