bc5e2880c479c588c1484e192fa07f574ed04da9
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-03-07 10:40:11 -0300

    curso web design sessão27_Event aula270

--- programming_logic/session_27/script/aula268.js
+++ programming_logic/session_27/script/aula268.js
@@ -1,17 +1,27 @@
 (function () {
    'use strict';
 
-   let $restRow = document.getElementById('restRow');
    let $txtMsg = document.getElementById('message');
+   let $restRow = document.getElementById('restRow');
    let $rest = document.getElementById('rest');
    let maximum = $txtMsg.maxLength;
 
-   $restRow.style.display = 'block';
-   $rest.textContent = maximum;
+   showContainer();
+   showLeft(maximum);
    $txtMsg.addEventListener('input', checkLength);
+
+   function showContainer() {
+      $restRow.style.display = 'block';
+   }
 
    function checkLength(event) {
       let nmrLetterTyping = this.value.length;
-      $rest.textContent = maximum - nmrLetterTyping;
+      let calc = parseInt(maximum) - parseInt(nmrLetterTyping);
+      let missingCharacter = calc;
+      showLeft(missingCharacter);
+   }
+
+   function showLeft(e) {
+      $rest.textContent = e;
    }
 })()
