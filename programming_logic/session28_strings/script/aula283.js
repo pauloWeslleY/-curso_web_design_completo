1967c1e55b435b61f7d72a976db76718f052a085
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-03-09 18:57:54 -0300

    curso web design sessão28 aula283

---
+++ programming_logic/session28_strings/script/aula283.js
@@ -0,0 +1,22 @@
+var testOne = 'my string is cool';
+
+console.log(testOne.length);
+console.log(testOne.replace('m', 'M'));
+
+// line
+console.log('|>--------------------------------------------');
+/*
+   ? indexOf
+*/
+
+console.log(testOne.indexOf(''));
+
+console.log(testOne.lastIndexOf(''));
+console.log(testOne.slice(5));
+console.log(testOne.slice(5, 10));
+console.log(testOne.substring(10, 5));
+console.log(testOne.slice(10, 5));
+console.log(testOne.split(' '));
+console.log(testOne);
+console.log(testOne.split('i'));
+console.log(testOne.toUpperCase());
