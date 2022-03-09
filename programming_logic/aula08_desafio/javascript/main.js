5b2bcdacc5fe94e608fc55b3dc128d330fd990b3
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-03-08 23:11:29 -0300

    curso web design sessão27 aula282

--- programming_logic/aula08_desafio/javascript/main.js
+++ programming_logic/aula08_desafio/javascript/main.js
@@ -2,10 +2,18 @@
    let html = document.querySelector('html');
    let btn = document.querySelector('.header-nav__hamburgger');
 
+   let ariaControl = btn.getAttribute('aria-controls');
+   console.log(ariaControl);
+
+   let menu = document.getElementById(ariaControl);
+
    html.classList.remove('no-js');
    html.classList.add('js');
 
-   html.addEventListener('click', function () {
+   btn.addEventListener('click', function () {
       html.classList.toggle('menu-opened');
+      let ariaExpanded = this.getAttribute('aria-expanded') === 'true';
+      btn.setAttribute('aria-expanded', !ariaExpanded);
+      menu.setAttribute('aria-expanded', !ariaExpanded);
    });
 })()
