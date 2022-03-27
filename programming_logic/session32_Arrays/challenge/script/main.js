ffba2ce300fad6bb6f2b40be49c602bfcbb783b8
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-03-27 20:28:58 -0300

    curso web design sessão32 aula301

--- programming_logic/session32_Arrays/challenge/script/main.js
+++ programming_logic/session32_Arrays/challenge/script/main.js
@@ -1,14 +1,7 @@
-var paragraphs = document.querySelectorAll('p');
+let paragraphs = document.querySelectorAll('p');
 
-for (var i = 0; i < paragraphs.length; i++){
-   paragraphs[i].innerHTML = formatIsParagraphs(paragraphs[i].innerHTML);
+for (let i = 0; i < paragraphs.length; i++){
+   let haveJS = paragraphs[i].textContent.toLowerCase().indexOf('javascript') >= 0;
+   paragraphs[i].className = haveJS ? 'destach' : '';
 }
 
-function formatIsParagraphs(isText) {
-   var isPhrasel = isText.split(' ');
-   console.log(isPhrasel);
-   if (isPhrasel.length > 102){
-      return isPhrasel.slice(0, 102).join(' ') + '...';
-   }
-   return isText;
-}
