3f2cfe30ce2eedc4fb4b03ea9bb6b71ed6468572
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-03-29 14:19:37 -0300

    curso web design sessão33 aula305

--- programming_logic/session33_Date/challenge33/js/index33.js
+++ programming_logic/session33_Date/challenge33/js/index33.js
@@ -22,4 +22,47 @@
    const ONE_DAY_TS = 24 * 60 * 60 * 1000;
    let different = dataBirthTs - dataCurrentTs;
    console.log(different / ONE_DAY_TS);
+
+
+
+   /*
+      ? Resolução Challenge |> Aula 305
+   */
+   //line
+   console.log('==>----------------------<==');
+
+   let date = new Date();
+   date.setDate(2);
+   date.setMonth(2);
+
+   function formateDate(date) {
+      let day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+      let month = date.getMonth() + 1;
+      month = month < 10 ? '0' + month : month;
+      let year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+      // return day + '/' + month + '/' + date.getFullYear();
+      // return `${date.getDate()}/${(date.getMonth() + 1)}/${date.getFullYear()}`
+   }
+
+   console.log(formateDate(date));
+
+   console.log('==>----------------------<==');
 })();
+
+const dateCurrent = new Date();
+
+const formatData = () => {
+   return dateCurrent.toLocaleDateString(
+      'pt-br',
+      {
+         day: '2-digit',
+         month: '2-digit',
+         year: 'numeric'
+      }
+   )
+}
+
+console.log(formatData());
+
+
