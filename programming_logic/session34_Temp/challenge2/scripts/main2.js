2f8c105b0c1cf8d27e1aaf0ff522b50ee0bb6e2f
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-03-31 17:24:57 -0300

    curso web design sessão34 aula311

--- programming_logic/session34_Temp/challenge2/scripts/main2.js
+++ programming_logic/session34_Temp/challenge2/scripts/main2.js
@@ -21,8 +21,32 @@
       setInterval(() => {
          let timeStampNow = Date.now();
          let isDiferent = timeStampNow - timeStampInitial;
-         stopWatch.value = isDiferent;
+         stopWatch.value = formatIsTimeStamp(isDiferent);
       }, 100);
+   }
+   const formatIsTimeStamp = (timeStamp) => {
+      /*
+         ms = 900 => 900
+         ms = 1500 => 1:500
+         ms = 59000 => 59:000
+         ms = 61500 => 1:1:500
+      */
+
+      if (timeStamp < 1000) {
+         return timeStamp;
+      } else if (timeStamp < 60000) {
+         let second = timeStamp / 1000;
+         second = parseInt(second);
+         let hundredth = timeStamp - (second * 1000);
+         return `${second}:${hundredth}`;
+      } else {
+         let isMinutes = timeStamp / (60000);
+         isMinutes = parseInt(isMinutes); // 1minute
+         let second = timeStamp / 1000 - (isMinutes * 60); // 61.5 = 1.5
+         second = parseInt(second); // 1
+         let hundredth = timeStamp - (second * 1000) - (isMinutes * 60 * 1000);
+         return `${isMinutes}:${second}:${hundredth}`;
+      }
    }
 
    init.addEventListener('click', isInit);
