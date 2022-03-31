32c0ea4e5638952292815a64aa2b5f7c35fcea57
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-03-31 14:43:12 -0300

    curso web design sessão34 aula309

---
+++ programming_logic/session34_Temp/challenge2/scripts/main2.js
@@ -0,0 +1,11 @@
+(() => {
+   let stopWatch = document.querySelector('#stopWatch');
+   let init = document.querySelector('#init');
+   let stop = document.querySelector('#stop');
+   let pause = document.querySelector('#pause');
+
+   // let isSeconds = 0;
+   // setInterval(() => {
+   //    stopWatch.value = ++isSeconds;
+   // }, 1000);
+})()
