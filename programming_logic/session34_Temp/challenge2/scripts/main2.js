64ecafbcb00951079276d6294581a3e9793097d7
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-03-31 15:00:49 -0300

    curso web design sessão34 aula310

--- programming_logic/session34_Temp/challenge2/scripts/main2.js
+++ programming_logic/session34_Temp/challenge2/scripts/main2.js
@@ -4,6 +4,31 @@
    let stop = document.querySelector('#stop');
    let pause = document.querySelector('#pause');
 
+   let timeStampInitial = 0;
+
+   const isInit = () => {
+      timeStampInitial = Date.now();
+      initialStopWatch();
+   }
+   const isPause = () => {
+
+   }
+   const isStop = () => {
+
+   }
+
+   const initialStopWatch = () => {
+      setInterval(() => {
+         let timeStampNow = Date.now();
+         let isDiferent = timeStampNow - timeStampInitial;
+         stopWatch.value = isDiferent;
+      }, 100);
+   }
+
+   init.addEventListener('click', isInit);
+   pause.addEventListener('click', isPause);
+   stop.addEventListener('click', isStop);
+
    // let isSeconds = 0;
    // setInterval(() => {
    //    stopWatch.value = ++isSeconds;
