c2abe1d41cc1800bda02c8e4e48bf72b3f316fa2
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-04-02 20:03:38 -0300

    curso web design sessão34 aula317

---
+++ programming_logic/session35_BOM/js/aula_317.js
@@ -0,0 +1,46 @@
+/*
+   fixme Aula 317 ==> BOM
+   * innerWidth / innerHeight
+   * outerWidth / outerHeight
+   * opener
+   * pageXOffset / pageYOffset
+   * screenX / screenY
+
+   fixme ==> BOM --> Objeto History
+   * History
+   * .length,
+   * .go(),
+   * .back(),
+   * .forward()
+
+   fixme ==> BOM --> Objeto Location
+   * Location
+   * .hash,
+   * .host,
+   * .hostname,
+   * .href,
+   * .pathname,
+   * .port,
+   * .protocol,
+   * .search
+   ! Location
+   *  .assign(url)
+   *  .reload(),
+   *  .replace(url)
+
+   fixme ==> BOM --> Objeto Navigator
+   * Navigator
+   *  .appName,
+   *  .appVersion,
+   *  .userAgent
+   *  .platform
+   *  .cookieEnabled
+   *  .language
+   *  .mimeTypes
+   *  .plugins
+   fixme ==> BOM --> Objeto Screen
+   * screen
+   *  .availableWidth / availableHeight
+   *  .width / height
+
+*/
