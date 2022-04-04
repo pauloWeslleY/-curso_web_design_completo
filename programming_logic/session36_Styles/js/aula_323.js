72cced9571580133f79c5424b173d24f053bd609
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-04-04 19:05:25 -0300

    curso web design sessão35 aula323

---
+++ programming_logic/session36_Styles/js/aula_323.js
@@ -0,0 +1,32 @@
+(() => {
+   let isNav = document.querySelector('.nav');
+   let isLinks = document.querySelectorAll('li a');
+   const isMenuDestach = () => {
+      let isPosition = [];
+      for (let i = 0; i < isLinks.length; i++){
+         isPosition[i] = getYPosScroll(isLinks[i]);
+      }
+      console.log(isPosition);
+
+      let isCurrentLink = getLastLinkVisible(isPosition);
+      let isLinkActived = isNav.querySelector('.actived');
+      if(isLinkActived) isLinkActived.classList.remove('actived');
+      isCurrentLink.classList.add('actived');
+   }
+   const getYPosScroll = (isLinks) => {
+      let isTarget = document.querySelector(isLinks.getAttribute('href'));
+      let isPosYViewPort = isTarget.getBoundingClientRect().top;
+      return isPosYViewPort;
+   }
+   const getLastLinkVisible = (isPosition) => {
+      let isQuantity = isPosition.length;
+      while (isQuantity) {
+         isQuantity--;
+         if (isPosition[isQuantity] < 0) {
+            return isLinks[isQuantity];
+         }
+      }
+      return isLinks[0];
+   }
+   window.addEventListener('scroll', isMenuDestach);
+})();
