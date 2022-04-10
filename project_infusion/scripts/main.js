155dca4d806cc682d51be5b0439d1e7061c4bd73
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-04-10 11:18:47 -0300

    curso web design sessão37 projectInfusion aula347

--- project_infusion/scripts/main.js
+++ project_infusion/scripts/main.js
@@ -3,6 +3,12 @@
    isBody.classList.remove('no-js');
    isBody.classList.add('js');
 
-   let isBtnMenu = document.querySelector('.header__btn_menu');
-   isBtnMenu.removeAttribute('style');
+   // let isBtnMenu = document.querySelector('.header__btn_menu');
+   // isBtnMenu.removeAttribute('style');
+
+   let isMenu = new isMenuMobile({
+      container: '.header__nav',
+      toggleBtn: '.header__btn_menu',
+      widthEnabled: 1014
+   })
 })()
---
+++ project_infusion/scripts/menu.js
@@ -0,0 +1,42 @@
+function isMenuMobile (config){
+   this.nav = (typeof config.container === 'string') ? document.querySelector(config.container) : config.container;
+   this.btn = (typeof config.toggleBtn === 'string') ? document.querySelector(config.toggleBtn) : config.toggleBtn;
+   this.maxWidth = config.widthEnabled || false;
+   let _isOpened = false;
+   let _this = this;
+   this.btn.removeAttribute('style');
+
+   const isApplyStyle = (_isStyle) => {
+      Object.keys(_isStyle).forEach(isStl => {
+         _this.nav.style[isStl] = _isStyle[isStl];
+         // _this.nav.style.maxHeight = 'calc(100vh - 50px)';
+         // _this.nav.style.overflow = 'hidden';
+      });
+   }
+   const isCloseMenu = () => {
+      let _isStyle = {
+         maxHeight: '0px',
+         overflow: 'hidden'
+      }
+      isApplyStyle(_isStyle);
+      _isOpened = false;
+   }
+   const isOpenOrClose = () => {
+      if (!_isOpened) {
+         isOpenMenu()
+      } else {
+         isCloseMenu()
+      }
+   }
+   this.btn.addEventListener('click', isOpenOrClose);
+   isCloseMenu();
+   const isOpenMenu = () => {
+      let _isTop = this.nav.getBoundingClientRect().top + 'px'
+      let _isStyle = {
+         maxHeight: `calc(100vh - ${_isTop})`,
+         overflow: 'hidden'
+      }
+      isApplyStyle(_isStyle);
+      _isOpened = true;
+   }
+}
